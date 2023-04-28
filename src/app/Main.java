dcafd9afb920c53cb3aaaa50b096668b59cdb542
Yan Nalon <havercraft1@gmail.com>
2023-04-28 14:53:57 -0300

    Fix

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,16 @@
 package app;
 
 public class Main {
-
+	
+	// (estrutura de dados) grafo direcionado - Yan
+	
+	// (metodo) gerador de grafo - Andre
+	
+	// (metodo) implemetar um metodo autoral (naive) para encontrar os fechos transitivos diretos e inversos
+	// de qualquer vertice e dizer base/antibase
+	
+	// (metodo) implementar o metodo de warshall e dizer base/antibase
+	
+	// (metodo) teste dizendo tempo de execução usando 10, 100, 1000, 10000 e 100000 vertices
+	
 }
