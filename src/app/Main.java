f44400ebb21a67f94911c1797f67ffdf10b70229
Yan Nalon <havercraft1@gmail.com>
2023-04-29 02:20:42 -0300

    Estrutura basica implementada

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import business.GrafoDirecionado;
+
 public class Main {
 	
 	// (estrutura de dados) grafo direcionado - Yan
@@ -12,5 +14,13 @@ public class Main {
 	// (metodo) implementar o metodo de warshall e dizer base/antibase
 	
 	// (metodo) teste dizendo tempo de execução usando 10, 100, 1000, 10000 e 100000 vertices
+	
+	public static void main(String[] args) {
+		GrafoDirecionado gr = new GrafoDirecionado(10000);
+		
+		gr.gerarGrafo();
+		
+		gr.print();
+	}
 	
 }
--- src/business/GrafoDirecionado.java
+++ src/business/GrafoDirecionado.java
@@ -4,18 +4,49 @@ import java.util.LinkedList;
 
 public class GrafoDirecionado {
 	
-	LinkedList<LinkedList<Integer>> adj = new LinkedList<>();
+	private LinkedList<LinkedList<Integer>> grafo;
+	private int qtdVertices;
 	
-	public GrafoDirecionado(int vertices, int arestas) {
-		
-	}
-	
-	public void addAresta() {
-		
-	}
-	
-	public void 
-	
-	
+	public GrafoDirecionado(int qtdVertices) {
+		this.qtdVertices = qtdVertices;
+		this.grafo = new LinkedList<>();
 
+		for (int i = 0; i < this.qtdVertices; i++) {
+			grafo.add(new LinkedList<Integer>());
+		}
+	}
+	
+	public boolean existeVertice(int v) {
+		return grafo.get(v) != null;
+	}
+
+	public boolean existeAresta(int v, int w) {
+		return grafo.get(v).contains(w);
+	}
+	
+	public void addVertice(int v) {
+		if (!existeVertice(v)) {
+			grafo.set(v, new LinkedList<Integer>());
+		}
+	}
+	
+	public void addAresta(int v, int w) {
+		if (!existeAresta(v, w)) {
+			grafo.get(v).add(w);
+		}
+	}
+	
+	public LinkedList<Integer> getVizinhanca(int v) {
+		return existeVertice(v) ? this.grafo.get(v) : null;
+	}
+	
+	public void gerarGrafo() {
+		// Andre: conectar vertices gerando arestas
+	}
+	
+	public void print() {
+		for (int i = 0; i < qtdVertices; i++) {
+			System.out.println(i + ": " + grafo.get(i).toString());
+		}
+	}
 }
