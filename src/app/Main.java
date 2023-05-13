c4914d189d0bee26b05421bfc0d93da42adb1b5c
André Rodrigues <mrnexusrider@hotmail.com>
2023-05-13 11:19:34 -0300

    Update DFS e menu MAIN

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,24 +1,45 @@
 package app;
 
+import java.rmi.server.SocketSecurityException;
+
 import business.GrafoDirecionado;
+import java.util.Scanner;
 
 public class Main {
-	
+
 	// OK - (estrutura de dados) grafo direcionado - Yan
-	
+
 	// OK - (metodo) gerador de grafo - Andre
-	
-	// (metodo) implemetar um metodo autoral (naive) para encontrar os fechos transitivos diretos e inversos
+
+	// (metodo) implemetar um metodo autoral (naive) para encontrar os fechos
+	// transitivos diretos e inversos
 	// de qualquer vertice e dizer base/antibase
-	
+
 	// (metodo) implementar o metodo de warshall e dizer base/antibase
-	
-	// (metodo) teste dizendo tempo de execução usando 10, 100, 1000, 10000 e 100000 vertices
-	
+
+	// (metodo) teste dizendo tempo de execução usando 10, 100, 1000, 10000 e 100000
+	// vertices
+
 	public static void main(String[] args) {
-		GrafoDirecionado gr = new GrafoDirecionado(100000, 2);
-		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Você deseja criar um grafo com quantos vértices? ");
+		int qtdVertices = sc.nextInt();
+		System.out.println("Você deseja que cada vertice do seu grafo tenha quantas arestas? ");
+		int qtdArestas = sc.nextInt();
+
+		GrafoDirecionado gr = new GrafoDirecionado(qtdVertices, qtdArestas);
+
+		System.out.println("Grafo:");
 		gr.print();
+
+		System.out.println("--------------------------");
+
+		System.out.println("Você deseja realizar uma busca em profundidade a partir de qual vértice?");
+		int verticeInicial = sc.nextInt();
+
+		gr.buscaProfundidade(verticeInicial);
+
 	}
-	
+
 }
--- src/business/GrafoDirecionado.java
+++ src/business/GrafoDirecionado.java
@@ -3,12 +3,12 @@ package business;
 import java.util.Arrays;
 
 public class GrafoDirecionado {
-	
+
 	private int qtdVertices, limiteArestas;
 	private int[][] grafo;
-	
+
 	// - 0 1 2 3 4 5 6 7 8 9
-	// 0 - - - - - - - - - - 
+	// 0 - - - - - - - - - -
 	// 1 - - - - - - - - - -
 	// 2 - 1 - - 1 - - - - -
 	// 3 - - 1 - 1 - - - - -
@@ -18,34 +18,34 @@ public class GrafoDirecionado {
 	// 7 - - - - - - - - - -
 	// 8 - - - - - - - - - -
 	// 9 - - - - - - - - - -
-	
+
 	public GrafoDirecionado(int qtdVertices, int limiteArestas) {
 		if (limiteArestas > qtdVertices) {
 			System.out.println("O numero maximo de arestas por vertice não pode ser maior que o numero de vertices");
 			return;
 		}
-		
+
 		this.qtdVertices = qtdVertices;
 		this.limiteArestas = limiteArestas;
-		
+
 		this.grafo = new int[qtdVertices][qtdVertices];
-		
+
 		gerarGrafo(this.grafo);
 	}
-	
+
 	public void gerarGrafo(int[][] grafo) {
-		
+
 		for (int i = 0; i < qtdVertices; i++) {
 			for (int j = 0; j < qtdVertices; j++) {
 				grafo[i][j] = 0;
 			}
 		}
-		
+
 		for (int i = 0; i < qtdVertices; i++) {
 			for (int j = 0; j < limiteArestas; j++) {
-				for(;;) {
-					int randomVertice = (int)(Math.random() * (qtdVertices));
-					
+				for (;;) {
+					int randomVertice = (int) (Math.random() * (qtdVertices));
+
 					if (grafo[i][randomVertice] != 1 && randomVertice != i) {
 						grafo[i][randomVertice] = 1;
 						break;
@@ -54,15 +54,16 @@ public class GrafoDirecionado {
 			}
 		}
 	}
-	
+
 	public void print() {
 		for (int i = 0; i < qtdVertices; i++) {
 			System.out.println(i + ": " + Arrays.toString(getVizinhanca(i)));
 		}
 	}
-	
+
 	public int[] getVizinhanca(int vertice) {
-		int[] vizinhanca = new int[limiteArestas]; int j = 0;
+		int[] vizinhanca = new int[limiteArestas];
+		int j = 0;
 		for (int i = 0; i < qtdVertices; i++) {
 			if (this.grafo[vertice][i] == 1) {
 				vizinhanca[j] = i;
@@ -71,4 +72,21 @@ public class GrafoDirecionado {
 		}
 		return vizinhanca;
 	}
+
+	public void buscaProfundidade(int verticeInicial) {
+		boolean[] visitados = new boolean[qtdVertices];
+		buscaProfundidade(verticeInicial, visitados);
+	}
+
+	public void buscaProfundidade(int vertice, boolean[] visitados) {
+		visitados[vertice] = true;
+		System.out.print(vertice + " ");
+
+		for (int i = 0; i < qtdVertices; i++) {
+			if (grafo[vertice][i] == 1 && !visitados[i]) {
+				buscaProfundidade(i, visitados);
+			}
+		}
+	}
+
 }
